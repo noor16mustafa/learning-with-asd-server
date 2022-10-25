af14a1ec62e17263d7887662691dd6cfef1e8e32
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-10-25 14:04:14 +0600

    courses get

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require("./data/course.json");
+
+
 app.get('/', (req, res) => {
     res.send(' API running');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log('Server running on port 5000');
