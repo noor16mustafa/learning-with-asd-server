32f9540d78819a4c4ef1520e55277ca8d8d11c55
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-10-25 14:02:49 +0600

    test server code added

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(' API running');
+});
+
+app.listen(port, () => {
+    console.log('Server running on port 5000');
+})
